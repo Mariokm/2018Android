aaa03b9d31ad8c0ba3f1cb691087be2e2064cec2
JLB <usuario@tuServerMail.com>
2018-09-07 15:57:26 -0700

    7 sep 2018

--- HolaMundoAndroid/app/src/main/java/com/example/itesca/lbeltran/holamundoandroid/MainActivity.java
+++ HolaMundoAndroid/app/src/main/java/com/example/itesca/lbeltran/holamundoandroid/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.itesca.lbeltran.holamundoandroid;
 
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    boolean estado = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.grid_layout);
+    }
+
+    public void funcionUno(View v){
+        TextView tv_uno = (TextView) findViewById(R.id.tv_uno);
+        estado = !estado;
+        tv_uno.setAllCaps(estado);
+    }
+    public void funcionDos(View v){
+        Context context = getApplicationContext();
+        CharSequence text = "Presionaste boton 2";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void funcionTres(View v){
+        Context context = getApplicationContext();
+        CharSequence text = "Presionaste boton 2";
+        int duration = Snackbar.LENGTH_LONG;
+        Snackbar.make(v,text,)
     }
 }
